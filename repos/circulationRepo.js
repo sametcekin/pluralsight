5cc611d291802e117f58b2cf2dd79c5040025c19
Samet Çekin <samet.cekin@logiwa.com>
2022-07-30 10:10:18 +0300

    get and getbyid methods are created.

--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -1,8 +1,47 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 function circulationRepo() {
   const url = "mongodb://localhost:27017";
   const dbName = "circulation";
+
+  function get(query, limit) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url);
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+
+        let items = db.collection("newspapers").find(query);
+
+        if (limit > 0) {
+          items = items.limit(limit);
+        }
+        resolve(await items.toArray());
+        client.close();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  function getById(id) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url);
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+
+        const item = await db
+          .collection("newspapers")
+          .findOne({ _id: ObjectId(id) });
+        resolve(item);
+        client.close();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
   function loadData(data) {
     return new Promise(async (resolve, reject) => {
       const client = new MongoClient(url);
@@ -13,13 +52,15 @@ function circulationRepo() {
         results = await db.collection("newspapers").insertMany(data);
 
         resolve(results);
+
+        client.close();
       } catch (error) {
         reject(error);
       }
     });
   }
 
-  return { loadData };
+  return { loadData, get, getById };
 }
 
 module.exports = circulationRepo();
